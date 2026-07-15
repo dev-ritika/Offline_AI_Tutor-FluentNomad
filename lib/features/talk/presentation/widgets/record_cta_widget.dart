823cef23a93cc30aae3165a85ea85bdafa390b01
Ritika <18.ritika.ritz@gmail.com>
2026-07-15 22:56:56 +0530

    animation to the mic cta

--- lib/features/talk/presentation/widgets/record_cta_widget.dart
+++ lib/features/talk/presentation/widgets/record_cta_widget.dart
@@ -1,19 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:offline_ai_tutor/core/utils/constants/color_consts.dart';
 
-class RecordCtaWidget extends StatelessWidget {
+class RecordCtaWidget extends StatefulWidget {
   final VoidCallback? callback;
+  final bool isTapped;
 
-  const RecordCtaWidget({super.key, this.callback});
+  const RecordCtaWidget({super.key, this.callback, this.isTapped = false});
+
+  @override
+  State<RecordCtaWidget> createState() => _RecordCtaWidgetState();
+}
+
+class _RecordCtaWidgetState extends State<RecordCtaWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> animation;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    animation = Tween<double>(
+      begin: 20 + Random().nextDouble() * 20,
+      end: 40 + Random().nextDouble() * 20,
+    ).animate(CurvedAnimation(parent: controller, curve: Curves.ease));
+
+    controller.forward();
+    controller.repeat();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Hero(
       tag: "record_cta",
       child: GestureDetector(
-        onTap: callback,
+        onTap: widget.callback,
 
-        child: const CircleAvatar(
+        child: CircleAvatar(
           backgroundColor: ColorConsts.buttonSecondaryColor,
           radius: 50,
           child: CircleAvatar(
@@ -22,7 +58,12 @@ class RecordCtaWidget extends StatelessWidget {
             child: CircleAvatar(
               backgroundColor: ColorConsts.buttonPLinearColor1,
               radius: 30,
-              child: Icon(Icons.mic, size: 30),
+              child: AnimatedBuilder(
+                animation: animation,
+                builder: (context, child) {
+                  return Icon(Icons.mic, size: animation.value);
+                },
+              ),
             ),
           ),
         ),
