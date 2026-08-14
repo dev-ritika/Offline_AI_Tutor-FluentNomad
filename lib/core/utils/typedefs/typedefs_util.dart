30b9d2a4a9c9dbae436acac1dd681a3dba4bf13e
Ritika <18.ritika.ritz@gmail.com>
2026-08-14 21:28:08 +0530

    added typedefs

--- lib/core/utils/typedefs/typedefs_util.dart
+++ lib/core/utils/typedefs/typedefs_util.dart
@@ -1,1 +1,9 @@
 import 'dart:core';
+
+import 'package:dartz/dartz.dart';
+import 'package:offline_ai_tutor/core/error_handling/failures.dart';
+
+typedef Result<T> = Either<Failures, T>;
+typedef ResultFuture<T> = Future<Either<Failures, T>>;
+typedef ResultStream<T> = Stream<Either<Failures, T>>;
+typedef DataMap = Map<String, dynamic>;
